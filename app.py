56c94bde9a28a02a734f009ab614cf23fc8eb5f4
ozknnberat <112058780+ozknnberat@users.noreply.github.com>
2025-11-22 02:16:09 +0000

    Hatalı kod yüklendi, test başlıyor

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+
+def veritabanina_baglan():
+    # GUVENLIK HATASI: Sifreyi kodun icine acikca yaziyoruz!
+    api_sifresi = "12345-COK-GIZLI-SIFRE"
+
+    if api_sifresi:
+        print("Baglanti basarili...")
+
+veritabanina_baglan()
